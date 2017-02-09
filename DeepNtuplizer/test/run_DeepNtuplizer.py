d88fb6fd305649fafa377af2d22e0276556f5028
Huilin Qu <huilin.qu@cern.ch>
2017-02-09 19:24:39 +0100

    Quark gluon likelihood.

--- DeepNtuplizer/test/run_DeepNtuplizer.py
+++ DeepNtuplizer/test/run_DeepNtuplizer.py
@@ -12,17 +12,38 @@ from Configuration.AlCa.GlobalTag import GlobalTag
 process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:run2_mc', '')
 #process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:run2_data', '')
 
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
-
 process.load('FWCore.MessageService.MessageLogger_cfi')
 process.MessageLogger.cerr.FwkReport.reportEvery = 1
 
+# VarParsing
+from FWCore.ParameterSet.VarParsing import VarParsing
+options = VarParsing('analysis')
+options.outputFile = 'output.root'
 from PhysicsTools.PatAlgos.patInputFiles_cff import filesRelValTTbarPileUpMINIAODSIM
-process.source = cms.Source("PoolSource",
-                            fileNames = filesRelValTTbarPileUpMINIAODSIM
-                            )
+options.inputFiles = filesRelValTTbarPileUpMINIAODSIM
+options.maxEvents = -1
+options.register('eventsToProcess',
+                  '',
+                  VarParsing.multiplicity.list,
+                  VarParsing.varType.string,
+                  "Events to process")
+options.parseArguments()
 
+process.maxEvents = cms.untracked.PSet(input=cms.untracked.int32(options.maxEvents))
+process.source = cms.Source('PoolSource',
+    fileNames=cms.untracked.vstring (options.inputFiles),
+)
+if options.eventsToProcess:
+    process.source.eventsToProcess = cms.untracked.VEventRange(options.eventsToProcess)
 
+# QGLikelihood
+process.load("DeepNTuples.DeepNtuplizer.QGLikelihood_cfi")
+process.es_prefer_jec = cms.ESPrefer("PoolDBESSource", "QGPoolDBESSource")
+process.load('RecoJets.JetProducers.QGTagger_cfi')
+process.QGTagger.srcJets   = cms.InputTag("slimmedJets")
+process.QGTagger.jetsLabel = cms.string('QGL_AK4PFchs')
+
+# DeepNtuplizer
 process.load("DeepNTuples.DeepNtuplizer.DeepNtuplizer_cfi")
 
-process.p = cms.Path(process.deepntuplizer)
+process.p = cms.Path(process.QGTagger + process.deepntuplizer)
