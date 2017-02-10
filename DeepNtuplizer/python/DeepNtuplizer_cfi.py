232419a89a590a31abf356343fc1fd900c41725b
Henning Kirschenmann <henning.kirschenmann@cern.ch>
2017-02-10 08:33:03 +0100

    add reclustered genjet pt, both with/without Nu

--- DeepNtuplizer/python/DeepNtuplizer_cfi.py
+++ DeepNtuplizer/python/DeepNtuplizer_cfi.py
@@ -3,6 +3,8 @@ import FWCore.ParameterSet.Config as cms
 deepntuplizer = cms.EDAnalyzer('DeepNtuplizer',
                                vertices   = cms.InputTag("offlineSlimmedPrimaryVertices"),
                                jets       = cms.InputTag("slimmedJets"),
+                               genJetMatchWithNu = cms.InputTag("patGenJetMatchWithNu"),
+                               genJetMatchRecluster = cms.InputTag("patGenJetMatchRecluster"),
                                jetPtMin     = cms.double(0.0),
                                jetPtMax     = cms.double(10000),
                                jetAbsEtaMin = cms.double(0.0),
--- DeepNtuplizer/test/run_DeepNtuplizer.py
+++ DeepNtuplizer/test/run_DeepNtuplizer.py
@@ -12,6 +12,40 @@ from Configuration.AlCa.GlobalTag import GlobalTag
 process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:run2_mc', '')
 #process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:run2_data', '')
 
+from RecoJets.JetProducers.ak4GenJets_cfi import ak4GenJets
+process.ak4GenJetsWithNu = ak4GenJets.clone(src = 'packedGenParticles')
+
+## Filter out neutrinos from packed GenParticles
+process.packedGenParticlesForJetsNoNu = cms.EDFilter("CandPtrSelector", src = cms.InputTag("packedGenParticles"), cut = cms.string("abs(pdgId) != 12 && abs(pdgId) != 14 && abs(pdgId) != 16"))
+## Define GenJets
+process.ak4GenJetsRecluster = ak4GenJets.clone(src = 'packedGenParticlesForJetsNoNu')
+
+
+process.patGenJetMatchWithNu = cms.EDProducer("GenJetMatcher",  # cut on deltaR; pick best by deltaR           
+    src         = cms.InputTag("slimmedJets"),      # RECO jets (any View<Jet> is ok) 
+    matched     = cms.InputTag("ak4GenJetsWithNu"),        # GEN jets  (must be GenJetCollection)              
+    mcPdgId     = cms.vint32(),                      # n/a   
+    mcStatus    = cms.vint32(),                      # n/a   
+    checkCharge = cms.bool(False),                   # n/a   
+    maxDeltaR   = cms.double(0.4),                   # Minimum deltaR for the match   
+    #maxDPtRel   = cms.double(3.0),                  # Minimum deltaPt/Pt for the match (not used in GenJetMatcher)                     
+    resolveAmbiguities    = cms.bool(True),          # Forbid two RECO objects to match to the same GEN object 
+    resolveByMatchQuality = cms.bool(False),         # False = just match input in order; True = pick lowest deltaR pair first          
+)
+
+process.patGenJetMatchRecluster = cms.EDProducer("GenJetMatcher",  # cut on deltaR; pick best by deltaR           
+    src         = cms.InputTag("slimmedJets"),      # RECO jets (any View<Jet> is ok) 
+    matched     = cms.InputTag("ak4GenJetsRecluster"),        # GEN jets  (must be GenJetCollection)              
+    mcPdgId     = cms.vint32(),                      # n/a   
+    mcStatus    = cms.vint32(),                      # n/a   
+    checkCharge = cms.bool(False),                   # n/a   
+    maxDeltaR   = cms.double(0.4),                   # Minimum deltaR for the match   
+    #maxDPtRel   = cms.double(3.0),                  # Minimum deltaPt/Pt for the match (not used in GenJetMatcher)                     
+    resolveAmbiguities    = cms.bool(True),          # Forbid two RECO objects to match to the same GEN object 
+    resolveByMatchQuality = cms.bool(False),         # False = just match input in order; True = pick lowest deltaR pair first          
+)
+
+
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 
 process.load('FWCore.MessageService.MessageLogger_cfi')
@@ -19,10 +53,10 @@ process.MessageLogger.cerr.FwkReport.reportEvery = 1
 
 from PhysicsTools.PatAlgos.patInputFiles_cff import filesRelValTTbarPileUpMINIAODSIM
 process.source = cms.Source("PoolSource",
-                            fileNames = filesRelValTTbarPileUpMINIAODSIM
-                            )
+   fileNames = filesRelValTTbarPileUpMINIAODSIM
+   )
 
 
 process.load("DeepNTuples.DeepNtuplizer.DeepNtuplizer_cfi")
 
-process.p = cms.Path(process.deepntuplizer)
+process.p = cms.Path(process.packedGenParticlesForJetsNoNu*process.ak4GenJetsWithNu*process.ak4GenJetsRecluster*process.patGenJetMatchWithNu*process.patGenJetMatchRecluster*process.deepntuplizer)
