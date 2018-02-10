ea24a538df8d31328e8cee0cc0324e5ac796ac5b
Emil Bols <emilbols@hotmail.com>
2018-02-10 14:27:26 +0100

    add extra dir

--- DeepNtuplizer/test/Steer.py
+++ DeepNtuplizer/test/Steer.py
@@ -6,23 +6,24 @@ outputdir = "/afs/cern.ch/work/e/ebols/public/hists/"
 
 datadir = ["JetHT/runB/171220_125115/0000", "JetHT/runC/171220_125350/0000", "JetHT/runC/171220_125350/0001", "JetHT/runD/171220_125424/0000", "JetHT/runE/171220_125459/0000", "JetHT/runE/171220_125459/0001", "JetHT/runF/171220_125533/0000", "JetHT/runF/171220_125533/0001"]
 
-mcdir = ["QCD_Pt_30to50_TuneCP5_13TeV_pythia8/incl_qcd_30/180117_082536/0000/", "QCD_Pt_50to80_TuneCP5_13TeV_pythia8/incl_qcd_50/180117_082611/0000/", "QCD_Pt_80to120_TuneCP5_13TeV_pythia8/incl_qcd_80/180117_082645/0000/", "QCD_Pt_120to170_TuneCP5_13TeV_pythia8/incl_qcd_120/180117_082718/0000/", "QCD_Pt_170to300_TuneCP5_13TeV_pythia8/incl_qcd_170/180117_082753/0000/", "QCD_Pt_300to470_TuneCP5_13TeV_pythia8/incl_qcd_300/180117_082827/0000/"]
+mcdir = ["QCD_Pt_30to50_TuneCP5_13TeV_pythia8/incl_qcd_30/180117_082536/0000/", "QCD_Pt_50to80_TuneCP5_13TeV_pythia8/incl_qcd_50/180117_082611/0000/", "QCD_Pt_80to120_TuneCP5_13TeV_pythia8/incl_qcd_80/180117_082645/0000/", "QCD_Pt_120to170_TuneCP5_13TeV_pythia8/incl_qcd_120/180117_082718/0000/", "QCD_Pt_170to300_TuneCP5_13TeV_pythia8/incl_qcd_170/180117_082753/0000/", "QCD_Pt_300to470_TuneCP5_13TeV_pythia8/incl_qcd_300/180117_082827/0000/","QCD_Pt_470to600_TuneCP5_13TeV_pythia8/incl_qcd_470/180117_082905/0000/"]
 
 root = '/eos/cms/store/group/phys_btag/gpaspala/Commissioning_2018/'
 
 outfile = "data"
 outend = ".root"
 
-blah = 0
+blah = 2936
 
-for root, dirs, files in os.walk(root+"JetHT/runB"):
+for root1, dirs, files in os.walk(root+"JetHT/runB"):
     for file in files:
         if file.endswith(".root"):
             index = str(blah)
-            call(["WeightDistrubtions", os.path.join(root,file), outputdir+outfile+index+outend, "DATA"])
+            call(["WeightDistrubtions", os.path.join(root1,file), outputdir+outfile+index+outend, "DATA"])
             blah += 1
             if blah%20 == 0:
                 print(blah)
+
 
 for x in mcdir:
     for file in os.listdir(root+x):
